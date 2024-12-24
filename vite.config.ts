f4313eff0501c81c25c00b3e83530ebfe193f18e
ZhenYi <434836402@qq.com>
2024-12-24 11:25:07 +0800

    feat Split build products

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
 import tsconfigPaths from "vite-tsconfig-paths";
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -11,8 +10,16 @@ export default defineConfig({
   ],
   build:{
     rollupOptions:{
-      external: ['compression']
-    }
+      external: ['compression'],
+      output:{
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      }
+    },
+    minify: "esbuild",
   },
   resolve: {
     alias: {
