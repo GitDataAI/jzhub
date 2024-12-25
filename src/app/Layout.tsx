13a35348711d15037849782e54c82328bf62a312
ZhenYi <434836402@qq.com>
2024-12-25 16:53:31 +0800

    op Page loading optimization

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -3,28 +3,42 @@ import {LayoutSidebar} from "@/component/layout/Sidebar.tsx";
 import {useInfo} from "@/store/useInfo.tsx";
 import {useUser} from "@/store/useUser.tsx";
 import {useNavigate} from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 export const Layout = () => {
     const nav = useNavigate();
     const info = useInfo();
     const user = useUser();
+    const [Show,setShow] = useState(false);
     useEffect(()=>{
         info.InitWorkspace().then().catch();
         if (!user.isLogin){
             nav("/auth/login")
         }else {
-            user.initial().then().catch();
+            user.initial().then(res=>{
+                if (!res){
+                    nav("/auth/login")
+                }else {
+                    setShow(true)
+                }
+            }).catch();
         }
     },[])
-    return(
-        <div onClick={()=>{
-            info.setModelShowId(0)
-        }}>
-            <LayoutHeader/>
-            <div className="layout-content">
-                <LayoutSidebar/>
+    if (Show){
+        return(
+            <div onClick={()=>{
+                info.setModelShowId(0)
+            }}>
+                <LayoutHeader/>
+                <div className="layout-content">
+                    <LayoutSidebar/>
+                </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return(
+            <>
+            </>
+        )
+    }
 }
