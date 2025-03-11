023f5348200d6deb442fdcb1c4eb69311348782d
ZhenYi <434836402@qq.com>
2025-03-11 08:22:31 +0800

    feat(server): Add ProductApi class to handle product-related API calls

---
+++ src/server/ProductApi.tsx
@@ -0,0 +1,16 @@
+import {HttpClient} from "@/server/Client";
+import {DataProductPostParam} from "@/server/types";
+
+export class ProductApi extends HttpClient {
+    async Post(owner: string, repo: string, param: DataProductPostParam) {
+        return await this.post<string>(`/product/${owner}/${repo}/post`, param)
+    }
+    async List(limit: number, page: number, order: string, search?: string) {
+        return await this.post<string>(`/product/list`, {
+            limit: limit,
+            page: page,
+            order: order,
+            search: search
+        })
+    }
+}
