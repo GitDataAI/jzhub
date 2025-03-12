77abc636de9f93eb675b1c5c5c2860cf08c800d5
ZhenYi <434836402@qq.com>
2025-03-12 17:01:16 +0800

    fix: Remove unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,9 @@ import '@mantine/core/styles.css';
 import "@/style/main.css"
 
 import {ColorSchemeScript, mantineHtmlProps, MantineProvider} from '@mantine/core';
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {Notifications} from "@mantine/notifications";
 import {CookieConsent} from "react-cookie-consent";
-import usePageContext from "@/store/usePageContext";
 
 
 export default function RootLayout(props: { children: React.ReactNode }) {
