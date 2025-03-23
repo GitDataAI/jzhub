19c1e9fc8ae891d4bf4d037e15de423c8e9d30a4
ZhenYi <434836402@qq.com>
2025-03-23 17:44:06 +0800

    dev

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
                 destination: "http://127.0.0.1:9000/api/v1/:path*",
                 basePath: false,
             },
+            {
+                source: "/git/:path*",
+                destination: "http://127.0.0.1:9000/git/:path*",
+                basePath: false,
+            },
         ];
     },
     output: "standalone",
