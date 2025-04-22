752765af229c483733f07c577b390ba461721fe0
ZhenYi <434836402@qq.com>
2025-04-22 23:43:54 +0800

    feat(explore): Complete the initial design of the explore page and data communication

--- src/app/(default)/explore/page.tsx
+++ src/app/(default)/explore/page.tsx
@@ -1,60 +1,109 @@
 'use client'
 
 import { useEffect, useState } from "react";
-import useUserContext from "@/store/useUserContext";
 import { notifications } from "@mantine/notifications";
-import {ExploreItem} from "@/component/explore/ExploreItem";
+import {
+    ExploreItemOrganization,
+    ExploreItemProduct,
+    ExploreItemUser
+} from "@/component/explore/ExploreItem";
 import { ExploreItemHeader } from "@/component/explore/ExploreItemHeader";
+import {Pagination} from "@mantine/core";
 
-export interface ExplorePageProps {
-    code: number,
-    data: ExploreModel[],
-    msg: string,
+export type ExplorePageProps =
+    undefined
+    | ExploreProduct
+    | ExploreUser
+    | ExploreOrganization;
+
+export interface ExploreProduct {
+    total: number,
+    type: string,
+    data: {
+        created_at: string,
+        default_branch: string,
+        description?: string,
+        fork: number,
+        star: number,
+        watch: number,
+        name: string,
+        rtype: string,
+        topic: string[],
+        uid: string,
+        updated_at: string,
+        owner: {
+            avatar: string,
+            description?: string,
+            uid: string,
+            name: string,
+        }
+    }[]
 }
 
-export interface ExploreModel {
-    id: string,
-    title: string,
-    description: string,
-    image: string,
-    created_at: string,
+export interface ExploreUser {
+    total: number,
+    type: string,
+    data: {
+        created_at: string,
+        description?: string,
+        name: string,
+        uid: string,
+        avatar?: string,
+        updated_at: string,
+    }[]
+}
+
+export interface ExploreOrganization {
+    total: number,
+    type: string,
+    data: {
+        created_at: string,
+        description?: string,
+        name: string,
+        avatar?: string,
+        uid: string,
+        updated_at: string,
+        repo: number,
+        member: number,
+    }[]
 }
 
 export default function ExplorePage() {
-    const user = useUserContext();
-    const [Data, setData] = useState<ExploreModel[]>([]);
+    const [Data, setData] = useState<ExplorePageProps>();
+    const [Rtype,setRtype] = useState<undefined | "product" | "user" | "organization">();
+    const [Filter, setFilter] = useState<undefined | 'public' | 'private'>();
+    const [Search, setSearch] = useState<string>("");
+    const [Page, setPage] = useState<{
+        page: number,
+        size: number,
+    }>({
+        page: 1,
+        size: 10,
+    })
 
     const FetchData = async () => {
-        if (!user.data) {
-            notifications.show({
-                title: 'Failed',
-                message: 'Please login first',
-                color: 'red',
-            });
-            return;
+        const payload = {
+            type: Rtype,
+            filter: Filter,
+            search: Search,
         }
-
-        const response = await fetch("/api/v1/users/explore", {
-            method: "POST",
+        const parma = new URLSearchParams();
+        parma.append("rtype", payload.type ? payload.type : "product");
+        parma.append("filter", payload.filter ? payload.filter : "all");
+        parma.append("search", payload.search);
+        parma.append("page", Page.page.toString())
+        parma.append("size", Page.size.toString())
+        const response = await fetch(`/api/v1/explore?${parma}`, {
+            method: "GET",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({
-                unix: parseInt(String(new Date().getTime() / 1000)),
-                device: "N/A",
-            }),
         });
 
         if (response.status === 200) {
             response.json().then((result: ExplorePageProps) => {
-                if (result.code === 0) {
-                    setData(result.data);
-                } else {
-                    notifications.show({
-                        title: 'Failed',
-                        message: result.msg,
-                        color: 'red',
-                    });
+                if(result) {
+                    setData(result);
                 }
             });
         } else {
@@ -68,16 +117,83 @@ export default function ExplorePage() {
 
     useEffect(() => {
         FetchData().then().catch().finally();
-    }, []);
-
+    },
+        // !!!forbid insert FetchData in useEffect deps
+        [Rtype, Filter, Search]);
     return (
         <div className="explore">
-            <ExploreItemHeader />
+            <ExploreItemHeader setRtype={(x)=>{
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-expect-error
+                setRtype(x)
+            }} rtype={Rtype} Filter={Filter} setFilter={(x)=>{
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-expect-error
+                setFilter(x)
+            }} search={Search} setSearch={(x)=>{
+                setSearch(x)
+            }}
+            />
             <div className="explore-list">
-                {Data.map((item, index) => (
-                    <ExploreItem key={index} data={item} />
-                ))}
+                {
+                    Data?.type === "product" &&
+                    Data?.data?.map((item, index) => (
+                        <ExploreItemProduct props={item as {
+                            created_at: string,
+                            default_branch: string,
+                            description?: string,
+                            fork: number,
+                            star: number,
+                            watch: number,
+                            name: string,
+                            rtype: string,
+                            topic: string[],
+                            uid: string,
+                            updated_at: string,
+                            owner: {
+                                avatar: string,
+                                description?: string,
+                                uid: string,
+                                name: string,
+                            }
+                        }} key={index}/>
+                    ))
+                }
+                {
+                    Data?.type === "user" &&
+                    Data?.data?.map((item, index) => (
+                        <ExploreItemUser props={item as {
+                            created_at: string,
+                            description?: string,
+                            name: string,
+                            uid: string,
+                            avatar?: string,
+                            updated_at: string,
+                        }} key={index}/>
+                    ))
+                }
+                {
+                    Data?.type === "organization" &&
+                    Data?.data?.map((item, index) => (
+                        <ExploreItemOrganization props={item as {
+                            created_at: string,
+                            description?: string,
+                            name: string,
+                            avatar?: string,
+                            uid: string,
+                            updated_at: string,
+                            repo: number,
+                            member: number,
+                        }} key={index}/>
+                    ))
+                }
             </div>
+            <Pagination className="explore-pagin" total={Data?.total ? Math.ceil(Data?.total / Page.size) : 1} color={"gray"} onChange={(x)=>{
+                setPage({
+                    page: x,
+                    size: Page.size,
+                })
+            }}/>
         </div>
     );
 }
--- src/component/explore/ExploreItem.tsx
+++ src/component/explore/ExploreItem.tsx
@@ -1,12 +1,119 @@
 import React from 'react';
+import {CiLink, CiStar} from "react-icons/ci";
+import {GoRepo, GoRepoForked} from 'react-icons/go';
+import {LuUsers} from "react-icons/lu";
 
-export const ExploreItem = ({ data }: { data: { id: string; title: string; description: string; image: string; created_at: string } }) => {
-    return (
-        <div className="explore-item">
-            <h3>{data.title}</h3>
-            <p>{data.description}</p>
-            <img src={data.image} alt={data.title} style={{ width: '100%', height: 'auto' }} />
-            <p>Posted on: {data.created_at}</p>
+
+export interface ExploreItemProductProps {
+    props:{
+        created_at: string,
+        default_branch: string,
+        description?: string,
+        fork: number,
+        star: number,
+        watch: number,
+        name: string,
+        rtype: string,
+        topic: string[],
+        uid: string,
+        updated_at: string,
+        owner: {
+            avatar: string,
+            description?: string,
+            uid: string,
+            name: string,
+        }
+    }
+}
+
+
+export const ExploreItemProduct = ({props}:ExploreItemProductProps) => {
+    return(
+        <div className="explore-product">
+            <img className='explore-product-avatar' src={props.owner.avatar ?? '/default.webp'} alt={props.owner.name}/>
+            <div className="explore-product-title">
+                <div className="explore-product-title-name">
+                    {props.name}
+                </div>
+                <div className="explore-product-title-desc">
+                    {props.description}
+                </div>
+            </div>
+            <div className="explore-product-data">
+                <div className="explore-product-data-item">
+                    <CiStar />{props.star}
+                </div>
+                <div className="explore-product-data-item">
+                    <CiLink />{props.watch}
+                </div>
+                <div className="explore-product-data-item">
+                    <GoRepoForked />{props.fork}
+                </div>
+            </div>
         </div>
-    );
-};
+    )
+}
+
+export interface ExploreItemUserProps {
+    props:{
+        created_at: string,
+        description?: string,
+        name: string,
+        uid: string,
+        avatar?: string,
+        updated_at: string,
+    }
+}
+
+export const ExploreItemUser = ({props}:ExploreItemUserProps) => {
+    return(
+        <div className="explore-users">
+            <img className='explore-users-avatar' src={props.avatar ?? '/default.webp'} alt={props.name}/>
+            <div className="explore-users-title">
+                <div className="explore-users-title-name">
+                    {props.name}
+                </div>
+                <div className="explore-users-title-desc">
+                    {props.description}
+                </div>
+            </div>
+        </div>
+    )
+}
+export interface ExploreItemOrganizationProps {
+    props:{
+        created_at: string,
+        description?: string,
+        name: string,
+        avatar?: string,
+        uid: string,
+        updated_at: string,
+        repo: number,
+        member: number,
+    }
+}
+
+export const ExploreItemOrganization = ({props}:ExploreItemOrganizationProps) => {
+    return(
+        <div className="explore-organization">
+            <img className='explore-organization-avatar' src={props.avatar ?? '/default.webp'} alt={props.name}/>
+            <div className="explore-organization-title">
+                <div className="explore-organization-title-name">
+                    {props.name}
+                </div>
+                <div className="explore-organization-title-desc">
+                    {props.description}
+                </div>
+            </div>
+            <div className="explore-organization-data">
+                <div className="explore-organization-data-item">
+                    <GoRepo />{props.repo}
+                </div>
+                <div className="explore-organization-data-item">
+                    <LuUsers />{props.member}
+                </div>
+            </div>
+
+        </div>
+    )
+}
