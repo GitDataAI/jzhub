df8381424857cd52464cf31bd3b87b4d61fae347
ZhenYi <434836402@qq.com>
2025-04-23 00:21:28 +0800

    fix: Stop using this page

--- src/app/(repo)/[user]/[repo]/setting/page.tsx
+++ src/app/(repo)/[user]/[repo]/setting/page.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import RepoSetHeader from '@/component/repo/RepoSetHeader';
+// import RepoSetHeader from '@/component/repo/RepoSetHeader';
 
 
-export default function Repo(props: { owner: string, repo: string, setting: string }) {
+export default function Repo() {
     return (
         <div className="repo">
-            <RepoSetHeader owner={props.owner} repo={props.repo} setting={props.setting} />
+            {/*<RepoSetHeader owner={props.owner} repo={props.repo} setting={props.setting} />*/}
             <div>
-                <p>这里是 repo 的其他内容</p>
             </div>
         </div>
     );
